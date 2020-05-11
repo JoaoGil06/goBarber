92a67678b5172ea0ceb94ccfe5992815e2f30cac
joaogil06 <Joaogmonteiro06@gmail.com>
2020-05-11 20:29:21 +0100

    Backend Part01

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm'
 import User from '../models/User'
 
+import AppError from '../errors/AppError'
+
 import path from 'path'
 import uploadConfig from '../config/upload'
 
@@ -19,7 +21,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id)
 
     if(!user) {
-      throw new Error('Only authenticated users can change avatar.')
+      throw new AppError('Only authenticated users can change avatar.', 401)
     }
 
     if(user.avatar) {
